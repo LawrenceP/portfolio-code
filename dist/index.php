564b6a3cf05223d6d61a7a429b211610c70b1623
Lawrence Phillips <Lawrence Phillips>
2016-08-26 18:07:33 +0100

    tidying up

--- dist/index.php
+++ dist/index.php
@@ -45,7 +45,7 @@
 				<div class="portfolio-outer">
 					<a href="sites/inoubliable/" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/inoubliable.png" alt="" class="img-responsive">
+							<img src="dist/images/inoubliable.png" alt="inoubliable" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Inoubliable</h2>
 								<p>A personal project built using angularjs, SCSS, and HTML. This quiz will determine how proficient you are in French.</p>
@@ -58,7 +58,7 @@
 				<div class="portfolio-outer">
 					<a href="http://www.hutchhouse.com/our-work/london-review-of-books/" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/london-review-of-books.png" alt="" class="img-responsive">
+							<img src="dist/images/london-review-of-books.png" alt="london-review-of-books" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>London Review of Books Case Study</h2>
 								<p>Built using jQuery, SCSS, and HTML</p>
@@ -71,7 +71,7 @@
 				<div class="portfolio-outer">
 					<a href="http://www.hutchhouse.com/our-work/assettradex/" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/asset-tradex.png" alt="" class="img-responsive">
+							<img src="dist/images/asset-tradex.png" alt="assettradex" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Asset Tradex Case Study</h2>
 								<p>Built using jQuery, SCSS, and HTML</p>
@@ -84,7 +84,7 @@
 				<div class="portfolio-outer">
 					<a href="http://www.pacificproduce.co.uk/" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/pacific-site.png" alt="" class="img-responsive">
+							<img src="dist/images/pacific-site.png" alt="pacificproduce" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Pacific Produce Website</h2>
 								<p>Built using jQuery, SCSS, and HTML</p>
@@ -97,7 +97,7 @@
 				<div class="portfolio-outer">
 					<a href="sites/platformer" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/platformer.png" alt="" class="img-responsive">
+							<img src="dist/images/platformer.png" alt="platformer" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Platformer Game</h2>
 								<p>A personal project built using javascript, phaser.js and photoshop.</p>
@@ -110,7 +110,7 @@
 				<div class="portfolio-outer">
 					<a href="https://www.speakezee.org/speaker/search" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/speakezee.png" alt="" class="img-responsive">
+							<img src="dist/images/speakezee.png" alt="speakezee" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Speakezee Website</h2>
 								<p>I worked on the speakers section of the Speakezee website. Built using Drupal, MySQL, SCSS and Javascript</p>
@@ -132,6 +132,7 @@
 					<h1>CONTACT</h1>
 					<p>lawrenceandrewphillips@gmail.com</p>
 					<p>07500484182</p>
+					<p><a href="https://github.com/LawrenceP/">https://github.com/LawrenceP/</a></p>
 					<div class="arrow-container">
 						<span class="arrow arrow-up-work glyphicon glyphicon-circle-arrow-up"></span>
 					</div>
--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 				<div class="portfolio-outer">
 					<a href="sites/inoubliable/" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/inoubliable.png" alt="" class="img-responsive">
+							<img src="dist/images/inoubliable.png" alt="inoubliable" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Inoubliable</h2>
 								<p>A personal project built using angularjs, SCSS, and HTML. This quiz will determine how proficient you are in French.</p>
@@ -60,7 +60,7 @@
 				<div class="portfolio-outer">
 					<a href="http://www.hutchhouse.com/our-work/london-review-of-books/" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/london-review-of-books.png" alt="" class="img-responsive">
+							<img src="dist/images/london-review-of-books.png" alt="london-review-of-books" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>London Review of Books Case Study</h2>
 								<p>Built using jQuery, SCSS, and HTML</p>
@@ -73,7 +73,7 @@
 				<div class="portfolio-outer">
 					<a href="http://www.hutchhouse.com/our-work/assettradex/" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/asset-tradex.png" alt="" class="img-responsive">
+							<img src="dist/images/asset-tradex.png" alt="assettradex" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Asset Tradex Case Study</h2>
 								<p>Built using jQuery, SCSS, and HTML</p>
@@ -86,7 +86,7 @@
 				<div class="portfolio-outer">
 					<a href="http://www.pacificproduce.co.uk/" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/pacific-site.png" alt="" class="img-responsive">
+							<img src="dist/images/pacific-site.png" alt="pacificproduce" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Pacific Produce Website</h2>
 								<p>Built using jQuery, SCSS, and HTML</p>
@@ -99,7 +99,7 @@
 				<div class="portfolio-outer">
 					<a href="sites/platformer" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/platformer.png" alt="" class="img-responsive">
+							<img src="dist/images/platformer.png" alt="platformer" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Platformer Game</h2>
 								<p>A personal project built using javascript, phaser.js and photoshop.</p>
@@ -112,7 +112,7 @@
 				<div class="portfolio-outer">
 					<a href="https://www.speakezee.org/speaker/search" target="_blank">
 						<div class="portfolio-img portfolio-inner">
-							<img src="dist/images/speakezee.png" alt="" class="img-responsive">
+							<img src="dist/images/speakezee.png" alt="speakezee" class="img-responsive">
 							<div class="portfolio-overlay text-center">
 								<h2>Speakezee Website</h2>
 								<p>I worked on the speakers section of the Speakezee website. Built using Drupal, MySQL, SCSS and Javascript</p>
@@ -134,6 +134,7 @@
 					<h1>CONTACT</h1>
 					<p>lawrenceandrewphillips@gmail.com</p>
 					<p>07500484182</p>
+					<p><a href="https://github.com/LawrenceP/">https://github.com/LawrenceP/</a></p>
 					<div class="arrow-container">
 						<span class="arrow arrow-up-work glyphicon glyphicon-circle-arrow-up"></span>
 					</div>
